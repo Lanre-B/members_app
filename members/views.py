935b2eef422217ac70c76abebc5ba834197f3c3f
olanrewajubakare <bakarevo@gmail.com>
2023-07-31 21:46:15 +0100

    first commit

---
+++ members/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.template import loader
+from .models import Member
+
+
+def members(request):
+    my_members = Member.objects.all().values()
+    my_members_name = Member._meta.verbose_name
+    template = loader.get_template("all_members.html")
+    context = {"my_members": my_members}
+    context["page_title"] = my_members_name.capitalize()
+    print(context)
+
+    return HttpResponse(template.render(context, request))
