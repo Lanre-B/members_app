4641657ec11fb94fb3714f7ca9d6a9bfaa0eb69d
olanrewajubakare <bakarevo@gmail.com>
2023-08-13 13:21:23 +0100

    created page 2

--- members/urls.py
+++ members/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("members/", views.members, name="members")]
+urlpatterns = [
+    path("members/", views.members, name="members"),
+    path('members/details/<int:pk>', views.details, name="details"),
+]
+
--- members/views.py
+++ members/views.py
@@ -6,9 +6,13 @@ from .models import Member
 def members(request):
     my_members = Member.objects.all().values()
     my_members_name = Member._meta.verbose_name
-    template = loader.get_template("details.html")
-    context = {"my_members": my_members}
-    context["page_title"] = my_members_name.capitalize()
-    print(context)
+    template = loader.get_template("all_members.html")
+    context = {"my_members": my_members, "page_header": my_members_name.capitalize()}
+    return HttpResponse(template.render(context, request))
 
+
+def details(request, pk):
+    my_members = Member.objects.get(id=pk)
+    template = loader.get_template('details.html')
+    context = {"my_members": my_members}
     return HttpResponse(template.render(context, request))
