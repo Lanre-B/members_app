d46c4a621aba7c66af9fbd802ee49fd401a9c51b
olanrewajubakare <bakarevo@gmail.com>
2023-08-13 07:39:36 +0100

    using the right template now

--- members/views.py
+++ members/views.py
@@ -6,7 +6,7 @@ from .models import Member
 def members(request):
     my_members = Member.objects.all().values()
     my_members_name = Member._meta.verbose_name
-    template = loader.get_template("all_members.html")
+    template = loader.get_template("details.html")
     context = {"my_members": my_members}
     context["page_title"] = my_members_name.capitalize()
     print(context)
